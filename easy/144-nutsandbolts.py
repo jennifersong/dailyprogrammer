1f82c4082ee3e97ab111d0813e92555df32e00f4
Jennifer Song <unorganize@gmail.com>
2013-12-14 02:34:02 -0800

    Update #144 Easy with problem description

--- easy/144-nutsandbolts.py
+++ easy/144-nutsandbolts.py
@@ -1,3 +1,14 @@
+#####################################################################
+# 
+#	ORIGINAL PROBLEM:
+#	Given two lists of N items and prices, print the items that have
+#	changed in price and the amount that the price has changed per item.
+#
+#	For more information, see the original prompt at 
+#	http://www.reddit.com/r/dailyprogrammer/comments/1sob1e/121113_challenge_144_easy_nuts_bolts/
+#
+#####################################################################
+
 numItems = int(raw_input())
 
 oldPrices = {item:int(price) for (item, price) in [raw_input().split(" ") for x in range(numItems)]}
