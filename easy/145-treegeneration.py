2f39e13f8f0a13dedc02f3e45ab1956850678fca
Jennifer Song <unorganize@gmail.com>
2013-12-17 01:19:18 -0800

    Add Easy Challenge #145 - Tree Generation

--- easy/145-treegeneration.py
+++ easy/145-treegeneration.py
@@ -2,7 +2,7 @@
 # 
 #        ORIGINAL PROBLEM:
 #        Draw an ASCII centered-triangle tree with a 1x3 character 
-# 		 trunk and a base of N characters, reducing each row by 2 characters.
+# 	 trunk and a base of N characters, reducing each row by 2 characters.
 #
 #        For more information, see the original prompt at 
 #        http://www.reddit.com/r/dailyprogrammer/comments/1t0r09/121613_challenge_145_easy_tree_generation/
