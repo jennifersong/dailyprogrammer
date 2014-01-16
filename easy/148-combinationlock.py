3a6ecf5bd8b6dddbabddef80d61e0fd775a64c2c
Jennifer Song <jehsong@gmail.com>
2014-01-16 00:01:32 -0800

    Add Easy Challenge #148 - Combination Lock

---
+++ easy/148-combinationlock.py
@@ -0,0 +1,28 @@
+#####################################################################
+# 
+#        ORIGINAL PROBLEM:
+#        Given the number of digits in a combination lock and the three
+#		 digit-code, print the number of rotations needed to open the lock.
+#
+#        For more information, see the original prompt at 
+#        http://www.reddit.com/r/dailyprogrammer/comments/1v4cjd/011314_challenge_148_easy_combination_lock/
+#
+#####################################################################
+def clockwise(old, new, num_digits):
+	return (new - old) % num_digits
+	
+def counterclockwise(old, new, num_digits):
+	return (old - new) % num_digits
+	
+def full_rotation(num_digits):
+	return num_digits
+
+def combination_lock():
+	digits = map(int, raw_input().split())
+	n, first, second, third = digits[0], digits[1], digits[2], digits[3]
+
+	total = full_rotation(n)*3 + clockwise(0, first, n) + \
+	counterclockwise(first, second, n) + clockwise(second, third, n)
+	return total
+
+print combination_lock()
