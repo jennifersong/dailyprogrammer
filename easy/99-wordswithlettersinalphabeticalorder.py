3be8a896a7bbfeac3172f5be19accda7c155903a
Jennifer Song <jehsong@gmail.com>
2014-01-16 21:23:10 -0800

    Add Easy Challenge #99 - Words with Letters in Alphabetical Order

---
+++ easy/99-wordswithlettersinalphabeticalorder.py
@@ -0,0 +1,16 @@
+#####################################################################
+# 
+#        ORIGINAL PROBLEM:
+#        Given a list of words, return the number of words that have all
+#        of their letters in alphabetical order (e.g. "ghost" but not "cab").
+#
+#        For more information, see the original prompt at 
+#        http://www.reddit.com/r/dailyprogrammer/comments/101m7y/9172012_challenge_99_easy_words_with_letters_in/
+#
+#####################################################################
+import fileinput
+
+def is_alphabetized(string):
+	return ''.join(sorted(string)) == string
+	
+print len([word.strip() for word in fileinput.input() if is_alphabetized(word.strip())])
